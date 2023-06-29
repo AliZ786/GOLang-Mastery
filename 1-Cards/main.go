f21a708041c189e781bd612049f0e25d825f0318
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2023-06-29 01:15:20 -0400

    Added some basic fundamental files for the cards project

---
+++ 1-Cards/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Diamonds")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+	
+
+}
+
+func newCard() string {
+	return "Queen of Diamonds"
+}
